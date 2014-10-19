ab851fbeca9a831744782b60a8bd97857c5ab95e
David Chang <davidchchang@gmail.com>
2014-10-19 14:51:40 -0400

    Render the correct number of incomplete todos remaining. Step: http://emberjs.com/guides/getting-started/displaying-the-number-of-incomplete-todos/

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -1,10 +1,14 @@
 Todos.TodosController = Ember.ArrayController.extend({
     actions: {
-        createTodo: function() {
+        createTodo: function () {
             // Get the todo title set by the "New Todo" text field
             var title = this.get('newTitle');
-            if (!title) { return false; }
-            if (!title.trim()) { return; }
+            if (!title) {
+                return false;
+            }
+            if (!title.trim()) {
+                return;
+            }
 
             // Create the new Todo model
             var todo = this.store.createRecord('todo', {
@@ -18,5 +22,14 @@ Todos.TodosController = Ember.ArrayController.extend({
             // Save the new model
             todo.save();
         }
-    }
+    },
+
+    remaining: function () {
+        return this.filterBy('isCompleted', false).get('length');
+    }.property('@each.isCompleted'),
+
+    inflection: function () {
+        var remaining = this.get('remaining');
+        return remaining === 1 ? 'item' : 'items';
+    }.property('remaining')
 });
